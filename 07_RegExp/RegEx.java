7e0b9c76e40725896f2acbf525163b155ba0e6bf
Olga Golovash <149199978+OlBreeze@users.noreply.github.com>
2024-12-08 16:22:35 +0200

    Add files via upload

---
+++ 07_RegExp/RegEx.java
@@ -0,0 +1,65 @@
+
+public class RegEx {
+
+	public static boolean isThreeDigitNumber(String str) {
+		String regExp = "[1-9]\\d{2}";
+		return str.matches(regExp);
+	}
+
+	public static boolean isPositiveNumberLessThan300(String str) {
+		//0-299 0-9 | 10 - 99 |100 - 299
+		String regExp = "[1-9]?\\d|[12]\\d{2}";
+		return str.matches(regExp);
+	}
+
+	public static boolean isIsraelMobile(String str) {
+		//[prefix]5\\d[-?\\d]{7}
+		String regExp = "((\\+972-?|\\+\\(972\\)-?|\\(\\+972\\)-?)|\\(?0)5\\d\\)?(-?\\d){7}";
+		return str.matches(regExp);		
+	}
+
+	public static boolean isNumber(String str) {
+		String regExp = "-?(0|[1-9]\\d*)(\\.\\d+)?";
+		return str.matches(regExp);
+	}
+
+	public static boolean isPositiveLessThan1000(String str) {
+		//0 | 1 - 9 | 10 - 99 | 100 - 999
+		String regExp = "0|[1-9]\\d{0,2}";
+		return str.matches(regExp);
+	}
+
+	public static boolean isPositiveLessThanEquals255(String str) {
+		//0-255 0-9 10-99 100 - 199 200 - 249  250 -255
+		String regExp = "0|[1-9]\\d?|1\\d{2}|2[0-4]\\d|25[0-5]";
+		return str.matches(regExp);
+	}
+
+	public static boolean isEmailCoIlMailRu(String str) {
+		String regExp = "[^\\s,@]+@[a-zA-Z\\d-]+\\.(co\\.il|mail\\.ru)";
+		return str.matches(regExp);
+	}
+
+	public static boolean isArithmeticExpression(String str) {
+		String regExp = "\\s*\\d+(\\s*[+*/-]\\s*\\d+)*\\s*";
+		return str.matches(regExp);
+	}
+
+	public static boolean checkCreditCardNumber(String str) {
+		String regex = "\\d{8,16}";
+		return str.matches(regex);
+	}
+
+	public static boolean checkDateFormatEU(String str) {
+		//assertTrue(StringExpressions.checkDateFormatEU("25.01.2020")); 
+		// 01-09 12[\\d] 3[01]
+		String regex = "(0[1-9]|[12]\\d|3[01])\\.(0[1-9]|1[0-2])\\.\\d{4}";
+		return str.matches(regex);
+	}
+	
+	public static boolean checkDateFormatUS(String string) {
+		return string.matches("\\d{4}-((0[1-9])|(1[0-2]))-((0[1-9])|([12][0-9])|(3[01]))");
+	}
+	
+
+}
---
+++ 07_RegExp/RegExpTest.java
@@ -0,0 +1,198 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegExpTest {
+
+	@Test
+	void testThreeDigitNumber()
+	{
+		assertTrue(RegEx.isThreeDigitNumber("123"));
+		assertTrue(RegEx.isThreeDigitNumber("100"));
+		assertTrue(RegEx.isThreeDigitNumber("999"));
+		assertFalse(RegEx.isThreeDigitNumber("000"));
+		assertFalse(RegEx.isThreeDigitNumber("007"));
+		assertFalse(RegEx.isThreeDigitNumber("1"));
+		assertFalse(RegEx.isThreeDigitNumber("11"));
+		assertFalse(RegEx.isThreeDigitNumber("1111"));
+		assertFalse(RegEx.isThreeDigitNumber("-111"));
+		assertFalse(RegEx.isThreeDigitNumber("111.1"));
+		assertFalse(RegEx.isThreeDigitNumber("11a"));
+		assertFalse(RegEx.isThreeDigitNumber("111$"));
+		assertFalse(RegEx.isThreeDigitNumber("%111"));
+	}
+
+	@Test
+	void testIsPositiveNumberLessThan300()
+	{
+		assertTrue(RegEx.isPositiveNumberLessThan300("0"));
+		assertTrue(RegEx.isPositiveNumberLessThan300("1"));
+		assertTrue(RegEx.isPositiveNumberLessThan300("11"));
+		assertTrue(RegEx.isPositiveNumberLessThan300("111"));
+		assertTrue(RegEx.isPositiveNumberLessThan300("299"));
+		assertFalse(RegEx.isPositiveNumberLessThan300("-1"));
+		assertFalse(RegEx.isPositiveNumberLessThan300("001"));
+		assertFalse(RegEx.isPositiveNumberLessThan300("00"));
+		assertFalse(RegEx.isPositiveNumberLessThan300("1.1"));
+		assertFalse(RegEx.isPositiveNumberLessThan300("300"));
+		assertFalse(RegEx.isPositiveNumberLessThan300("aaa"));
+	}
+
+	@Test
+	void testIsraelMobile()
+	{
+		assertFalse(RegEx.isIsraelMobile("(+(972541234567"));
+		assertTrue(RegEx.isIsraelMobile("0541234567"));
+		assertTrue(RegEx.isIsraelMobile("055-1234567"));
+		assertTrue(RegEx.isIsraelMobile("059-123-4567"));
+		assertTrue(RegEx.isIsraelMobile("058-123-45-67"));
+		assertTrue(RegEx.isIsraelMobile("+972541234567"));
+		assertTrue(RegEx.isIsraelMobile("+972-54-123-4-5-6-7"));
+		assertTrue(RegEx.isIsraelMobile("(+972)541234567"));
+		assertTrue(RegEx.isIsraelMobile("+(972)541234567"));
+		assertTrue(RegEx.isIsraelMobile("(054)123-45-67"));
+		assertFalse(RegEx.isIsraelMobile("541234567"));
+		assertFalse(RegEx.isIsraelMobile("05412345679999"));
+		assertFalse(RegEx.isIsraelMobile("0641234567"));
+		assertFalse(RegEx.isIsraelMobile("054--1234567"));
+		assertFalse(RegEx.isIsraelMobile("0541234567-"));
+		assertFalse(RegEx.isIsraelMobile("972541234567"));
+		assertFalse(RegEx.isIsraelMobile("0541b34567"));
+		assertFalse(RegEx.isIsraelMobile("0541$34567"));
+	}
+
+	@Test
+	void testIsNumber()
+	{
+		assertTrue(RegEx.isNumber("0"));
+		assertTrue(RegEx.isNumber("-0"));
+		assertTrue(RegEx.isNumber("1"));
+		assertTrue(RegEx.isNumber("11"));
+		assertTrue(RegEx.isNumber("1111"));
+		assertTrue(RegEx.isNumber("11111111111"));
+		assertTrue(RegEx.isNumber("-1"));
+		assertTrue(RegEx.isNumber("-11"));
+		assertTrue(RegEx.isNumber("-1111"));
+		assertTrue(RegEx.isNumber("-11111111111"));
+		assertTrue(RegEx.isNumber("0.1"));
+		assertTrue(RegEx.isNumber("-0.1"));
+		assertTrue(RegEx.isNumber("1.1"));
+		assertTrue(RegEx.isNumber("-1.1"));
+		assertTrue(RegEx.isNumber("111.1"));
+		assertTrue(RegEx.isNumber("1111111.1111"));
+		assertFalse(RegEx.isNumber("1111111,1111"));
+		assertFalse(RegEx.isNumber("011"));
+		assertFalse(RegEx.isNumber("-011.1111"));
+		assertFalse(RegEx.isNumber("11..1111"));
+		assertFalse(RegEx.isNumber("-11.11.11"));
+		assertFalse(RegEx.isNumber("aaa"));
+		assertFalse(RegEx.isNumber("$1111"));
+		assertFalse(RegEx.isNumber("11 11"));
+		assertFalse(RegEx.isNumber("1111."));
+		assertFalse(RegEx.isNumber("1111-"));
+		assertFalse(RegEx.isNumber("-111-1"));
+		assertFalse(RegEx.isNumber("-.1111"));
+	}
+
+	@Test
+	void testIsPositiveLessThan1000()
+	{
+		assertTrue(RegEx.isPositiveLessThan1000("1"));
+		assertTrue(RegEx.isPositiveLessThan1000("0"));
+		assertTrue(RegEx.isPositiveLessThan1000("11"));
+		assertTrue(RegEx.isPositiveLessThan1000("111"));
+		assertTrue(RegEx.isPositiveLessThan1000("999"));
+		assertFalse(RegEx.isPositiveLessThan1000("1000"));
+		assertFalse(RegEx.isPositiveLessThan1000("-1"));
+		assertFalse(RegEx.isPositiveLessThan1000("-1.1"));
+		assertFalse(RegEx.isPositiveLessThan1000("1.1"));
+		assertFalse(RegEx.isPositiveLessThan1000("sss"));
+		assertFalse(RegEx.isPositiveLessThan1000("1%"));
+		assertFalse(RegEx.isPositiveLessThan1000("1a"));
+		assertFalse(RegEx.isPositiveLessThan1000("001"));
+	}
+
+	@Test
+	void testIsPositiveLessThanEquals255()
+	{
+		assertTrue(RegEx.isPositiveLessThanEquals255("255"));
+		assertTrue(RegEx.isPositiveLessThanEquals255("1"));
+		assertTrue(RegEx.isPositiveLessThanEquals255("0"));
+		assertFalse(RegEx.isPositiveLessThanEquals255("256"));
+		assertFalse(RegEx.isPositiveLessThanEquals255("-255"));
+		assertFalse(RegEx.isPositiveLessThanEquals255("-1"));
+		assertFalse(RegEx.isPositiveLessThanEquals255("255.1"));
+		assertFalse(RegEx.isPositiveLessThanEquals255("25 5"));
+		assertFalse(RegEx.isPositiveLessThanEquals255("a"));
+	}
+
+	@Test
+	void testEmailAddress()
+	{
+		// there must be @
+		// before @ maybe any symbols except whitespace and comma
+		// after @ may be any letters or digits or dashes
+		// email address should ended either .co.il or .mail.ru
+		assertTrue(RegEx.isEmailCoIlMailRu("t%u@12.co.il"));
+		assertTrue(RegEx.isEmailCoIlMailRu("t#u@t.mail.ru"));
+		assertTrue(RegEx.isEmailCoIlMailRu("12_5-6*@y.co.il"));
+		assertTrue(RegEx.isEmailCoIlMailRu("tu@-12.co.il"));
+		assertFalse(RegEx.isEmailCoIlMailRu("t,u@12.co.il"));// , - disallowed
+		assertFalse(RegEx.isEmailCoIlMailRu("t u@12.co.il"));// space disallowed
+		assertFalse(RegEx.isEmailCoIlMailRu("ttu@1#2.co.il"));// after @ the symbol # disallowed
+		assertFalse(RegEx.isEmailCoIlMailRu("ttu@12.mail1ru"));// neither .co.il nor mail.ru
+		assertFalse(RegEx.isEmailCoIlMailRu("ttu@12co.il"));// neither .co.il nor mail.ru
+		assertFalse(RegEx.isEmailCoIlMailRu("ttu12.co.il"));// no @
+		assertFalse(RegEx.isEmailCoIlMailRu("ttu@1_2.co.il"));// after @ the symbol _ disallowed
+		
+	}
+
+	@Test
+	void testArithmeticExpression()
+	{
+	// operands (positive integer numbers), operators either with space or no space between
+		// Arithmetic operations + * / -
+		assertTrue(RegEx.isArithmeticExpression("2+3 /7"));
+		assertTrue(RegEx.isArithmeticExpression("2"));
+		assertTrue(RegEx.isArithmeticExpression("2*3 /7"));
+		assertTrue(RegEx.isArithmeticExpression(" 2 + 3 / 7"));
+		assertFalse(RegEx.isArithmeticExpression("*3 /7"));
+		assertFalse(RegEx.isArithmeticExpression("2..5 +8/2"));// ..
+		assertFalse(RegEx.isArithmeticExpression("2*5 +8#2"));// # isn't code operation
+	}
+
+	@Test
+	void testCheckCreditCardNumber()
+	{
+		assertTrue(RegEx.checkCreditCardNumber("12345678"));
+		assertTrue(RegEx.checkCreditCardNumber("1234567890"));
+		assertTrue(RegEx.checkCreditCardNumber("1234567887654321"));
+		assertFalse(RegEx.checkCreditCardNumber("12345678876543210"));
+		assertFalse(RegEx.checkCreditCardNumber("1234567"));
+		assertFalse(RegEx.checkCreditCardNumber("123456780A"));
+	}
+
+	@Test
+	void testCheckDateFormatEU()
+	{
+		assertTrue(RegEx.checkDateFormatEU("25.01.2020"));
+		assertTrue(RegEx.checkDateFormatEU("05.11.2018"));
+		assertFalse(RegEx.checkDateFormatEU("32.12.2020"));
+		assertFalse(RegEx.checkDateFormatEU("12.15.2020"));
+		assertFalse(RegEx.checkDateFormatEU("12/12/2020"));
+		assertFalse(RegEx.checkDateFormatEU("2020.12.15"));
+		assertFalse(RegEx.checkDateFormatEU("2020-12-15"));
+		assertFalse(RegEx.checkDateFormatEU("5.11.2018"));
+	}
+
+	@Test
+	void testCheckDateFormatUS()
+	{
+		assertTrue(RegEx.checkDateFormatUS("2020-12-15"));
+		assertTrue(RegEx.checkDateFormatUS("2020-05-21"));
+		assertFalse(RegEx.checkDateFormatUS("2020-05-32"));
+		assertFalse(RegEx.checkDateFormatUS("2020-15-30"));
+		assertFalse(RegEx.checkDateFormatUS("2020-121-30"));
+	}
+
+}
